3170223478a0dd7428d6930b23467b268c0aee40
Anton <kroshilin.a@yandex.ru>
2018-12-04 00:07:55 +0200

    Update run-while.sh

--- run-while.sh
+++ run-while.sh
@@ -10,6 +10,6 @@ trap cleanup SIGINT SIGTERM
 echo "Starting cron"
 while [ true ]
 do
-$@ # f.i. php /var/www/artisan schedule:run >> /dev/null 2>&1
+bash -c "$@" # f.i. php /var/www/artisan schedule:run >> /dev/null 2>&1
 sleep 60s
 done
